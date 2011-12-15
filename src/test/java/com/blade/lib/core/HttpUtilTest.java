c7b0360fdf4012ed6b27158588efe1870d945470
Chandra Sekar S <chandru.in@gmail.com>
2011-12-15 08:53:33 +0530

    Use NanoHTTPD to test HttpUtil.

--- src/test/java/com/blade/lib/core/HttpUtilTest.java
+++ src/test/java/com/blade/lib/core/HttpUtilTest.java
@@ -4,47 +4,55 @@ package com.blade.lib.core;
  *
  * @author avarakukalayil
  */
+import static org.testng.Assert.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.blade.lib.core.HttpUtil;
-import org.apache.log4j.BasicConfigurator;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import junit.framework.Assert;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
+import com.blade.testutil.NanoHTTPD;
 
 public class HttpUtilTest {
+	private NanoHTTPD server;
+	
+	@BeforeClass
+	public void setUp() throws IOException {
+    	server = new NanoHTTPD(9898, null) {
+			@Override
+			public Response serve(String uri, String method, Properties header,
+					Properties parms, Properties files) {
+				if("/get_path".equals(uri) && "GET".equals(method)) {
+					return new Response("200", "application/json", "{\"msg\":\"Get Response\"}");
+				}
 
-    @BeforeClass
-    public void setUp() {
-        BasicConfigurator.configure();
-    }
-
-    @AfterClass
-    public void cleanUp() {
-        // code that will be invoked after this test ends
-    }
+				if("/post_path".equals(uri) && "POST".equals(method)) {
+					Object msgParam = parms.get("msg");
+					return new Response("200", "application/json", "{\"msg\":\"" + msgParam + "\"}");
+				}
+				return new Response("404", "text/plain", "Not Found");
+			}
+    	};
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		server.stop();
+	}
 
     @Test
-    public void getRequestWithUri() throws Exception {
-        URI uri = new URI("http://www.google.com");
-        String response = HttpUtil.get(uri);
-        Assert.assertTrue(response.contains("Google"));
+    public void testGet() throws JSONException {
+    	JSONObject response = HttpUtil.get("http://localhost:9898/get_path");
+    	assertEquals(response.toString(), "{\"msg\":\"Get Response\"}");
     }
     
     @Test
-    public void postRequestWithUri() throws Exception {
-        URI uri = new URI("http://www.google.com");
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-        nameValuePairs.add(new BasicNameValuePair("id", "1"));
-        String response = HttpUtil.post(uri, nameValuePairs);
+    public void testPost() throws JSONException {
+    	JSONObject response = HttpUtil.post("http://localhost:9898/post_path", "msg=Hello");
+    	assertEquals(response.toString(), "{\"msg\":\"Hello\"}");
     }
-    
 }

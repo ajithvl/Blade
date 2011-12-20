a5ee5473603108fbd7bb07399c03026ebb408119
Ajith V L <avarakukalayil@zynga.com>
2011-12-20 18:18:37 +0530

    Renamed few packages and made some changes to pom.xml Deleted testng.xml as it should be delivered outside blade

--- src/main/java/com/blade/Launcher.java
+++ src/main/java/com/blade/Launcher.java
@@ -1,6 +1,6 @@
 package com.blade;
 
-
+import com.blade.core.NanoHTTPD;
 import com.blade.core.RuntimeReporter;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -11,6 +11,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import org.testng.ITestContext;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
@@ -23,11 +24,43 @@ import org.testng.xml.XmlSuite;
 public class Launcher {
     
     public static void main(String[] args) throws Exception{
-        TestListenerAdapter tla = new RuntimeReporter();
+        
+        /*
+         * Invoking HTTP server to listen to commands
+         * COMMANDS:
+         * 1) skip      : Skip all the tests in the list
+         * 2) abort     : Gracefully abort all the pending tests
+         */
+        
+        NanoHTTPD server = new NanoHTTPD(9898, null) {
+
+            @Override
+            public NanoHTTPD.Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
+                if ("/skip".equals(uri) && "POST".equals(method)) {
+                    Object msgParam = parms.get("msg");
+                    return new NanoHTTPD.Response("200", "application/json", "{\"msg\":\"Skipping\"}");
+                }
+                if ("/abort".equals(uri) && "POST".equals(method)) {
+                    Object msgParam = parms.get("msg");
+                    return new NanoHTTPD.Response("200", "application/json", "{\"msg\":\"Aborting\"}");
+                }
+                return new NanoHTTPD.Response("404", "text/plain", "Not Found");
+            }
+        };
+
+        
+       
+        
+        //TestListenerAdapter tla = new RuntimeReporter();
         TestNG testng = new TestNG();
-        testng.addListener(tla);
+        //testng.addListener(tla);
+        
         testng.setTestSuites(Arrays.asList("config/testng.xml"));
         testng.run();
         
+        
+        
+        // Stops the HTTP server
+        server.stop();
     }
 }
